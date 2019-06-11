762d096d200d5929dda49ee89cefa947dc22d9b4
saranya chandrasekar <saranyachandrasekar1@gmail.com>
2019-06-11 17:58:11 +0530

    Add all my files

--- src/App.js
+++ src/App.js
@@ -29,7 +29,12 @@ class App extends Component {
             <button onClick={this.props.AgeDown}>Age down</button>
             <ul>
               {
-                this.props.history.map(el=> <li onClick={()=>this.props.DelItem(el.id)} key={el.id}>{el.age}</li>)
+                this.props.history.map((el,index)=>{ return(
+                  <li  key={el.id}>{el.age}<button onClick={()=>this.props.EditItem(el.id,el.age)}>Edit</button><button onClick={()=>this.props.DelItem(el.id)}>Delete </button>{this.props.edited &&  this.props.editid === el.id && <input onChange={(e)=>this.props.OnEditItem(el.id,e)}type="number" ref="ageval"/>}<button onClick={()=>this.props.SaveItem(el.id,this.props.editedval,index)}>Save</button>
+                </li>
+                )
+              }
+                )
               }
             </ul>
         </div>
@@ -40,15 +45,22 @@ class App extends Component {
 }
 const mapStateToProps = (state)=>{
   return{
-    age: state.rUp.age,
-    history: state.history
+    age: state.age,
+    history: state.history,
+    edited:state.edited,
+    editedval : state.editedval,
+    editid:state.editid
+
   }
 }
 const mapDispatchToProps = (dispatch)=>{
   return{
     AgeUp : ()=>{dispatch({type:'AGE_UP',val : 1})},
     AgeDown : ()=>{dispatch({type:'AGE_DOWN',val : 1})},
-    DelItem: (id)=> {dispatch({type:'DELETE_ITEM',key:id})}
+    DelItem: (id)=> {dispatch({type:'DELETE_ITEM',key:id})},
+    EditItem: (id,val)=> {dispatch({type:'EDIT_ITEM',key:id,val:val})},
+    OnEditItem: (id,e)=> {dispatch({type:'ON_EDIT_ITEM',key:id,val:e.target.value})},
+    SaveItem: (id,ageval,index)=> {dispatch({type:'SAVE_ITEM',key:id,val:ageval,index:index})}
   }
 }
 export default connect(mapStateToProps,mapDispatchToProps)(App);
--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,6 +1,7 @@
 const intialState ={
     age :24,
-    history: []
+    history: [],
+    edited:false
 }
 const reducer = (state = intialState,action) =>{
     if(action.type === 'AGE_UP'){
@@ -21,6 +22,33 @@ const reducer = (state = intialState,action) =>{
         return{
             ...state,
             history: state.history.filter(el=> el.id !== action.key)
+        }
+    }
+    if(action.type === 'EDIT_ITEM'){
+        return{
+            ...state,
+            edited:true,
+            editid : action.key
+        }
+    }
+    if(action.type === 'ON_EDIT_ITEM'){
+        console.log(action.val)
+        return{
+            ...state,
+            editedval:action.val
+        }
+        
+    }
+    if(action.type === 'SAVE_ITEM'){
+        console.log(state);
+        console.log(state.history[action.index]);
+       
+        state.history[action.index].age = action.val
+        console.log(state.history);
+        return{
+            ...state,
+            history : state.history,
+            edited:false
         }
     }
     return state
